43f664848f9ba822e626d916d9b8473dca6a91f0
SamPassmore <passmore.sam@gmail.com>
2019-06-16 10:37:55 +0100

    new talks

--- build.R
+++ build.R
@@ -3,3 +3,6 @@ source('content/talk/write-talk-files.R')
 
 ## update publications
 #source()
+
+blogdown::build_dir()
+blogdown::serve_site()
--- public/talk/write-talk-files.R
+++ public/talk/write-talk-files.R
@@ -8,7 +8,8 @@ gs_auth()
 object = gs_url("https://docs.google.com/spreadsheets/d/1V-VWoaEWJAal20WTay_1wF9uCBJT8kOstegjNuWvLrU/edit#gid=0")
 ## Assuming I always want the first sheet 
 talks = gs_read(object, check.names = TRUE, ws = 1)
-talks = subset(talks, talks$Type == "Talk")
+talks = subset(talks, talks$Type == "Talk" | talks$Type == "Poster")
+today = Sys.Date()
 
 for(i in 1:nrow(talks)){
   row = talks[i,]
@@ -17,7 +18,9 @@ for(i in 1:nrow(talks)){
   date = lubridate::dmy(row$Date)
   year = lubridate::year(date)
   event = row$Event
+  type = row$Type
   location = row$Location
+  institute = row$Institute
   
   file = stringr::str_glue('+++
 title = "{title}"
@@ -32,7 +35,7 @@ authors = []
 event = "{event}"
            
 # Location of event.
-location = "{location}"
+location = "{institute}"
            
 # Is this a featured talk? (true/false)
 featured = false
@@ -40,13 +43,16 @@ featured = false
 +++
 ')
   
-  
+  institute_strip = institute %>% 
+    tolower() %>%
+    str_remove_all("[:space:]")
   fname = title %>% 
     str_extract_all('[:alnum:]') %>% 
     unlist(.) %>%
     paste0(., collapse = "") %>% 
-    paste0("content/talk/", year, ., ".md")
+    paste0("content/talk/", year, ., institute_strip,".md")
   fileConn<-file(fname)
-  writeLines(file, fileConn)
-  close(fileConn)
+  if(!file.exists(fname))
+    writeLines(file, fileConn)
+    close(fileConn)
 }
